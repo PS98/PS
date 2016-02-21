260fb5e4ea71c11b7bc20417195c6948b223c4f1
Shobhit Varshney <varshneyshobhit98@hotmail.com>
2016-02-21 20:14:02 +0530

    client side code structure

---
+++ src/PS/wwwroot/app/widgets/wwaEmployee/wwaEmployeeDirective.js
@@ -0,0 +1,28 @@
+﻿"use strict";
+
+angular.module("app").directive("wwaEmployee",
+    ["dataService",
+    function (dataService) {
+        return {
+            templateUrl: "app/widgets/wwaEmployee/wwaEmployeeTemplate.html",
+            link: function (scope, el, attrs) {
+                scope.selectedEmployee = null;
+                scope.isLoaded = false;
+                scope.hasError = false;
+
+                scope.loadEmployee = function () {
+                    scope.hasError = false;
+                        dataService.getEmployee(scope.item.widgetSettings.id)
+                    .then(function (data) {
+                        scope.selectedEmployee = data;
+                        scope.isLoaded = true;
+                        scope.hasError = false;
+                    }, function () {
+                        scope.hasError = true;
+                    });
+                }
+
+                scope.loadEmployee();
+            }
+        };
+    }]);
