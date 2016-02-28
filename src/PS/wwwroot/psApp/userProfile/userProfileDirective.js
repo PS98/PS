43276070fdc5c116124f75d6ceb6344057c81383
vikrant <vikrantnaik01@gmail.com>
2016-02-28 17:40:16 +0530

    Local Changes

--- src/PS/wwwroot/psApp/userProfile/userProfileDirective.js
+++ src/PS/wwwroot/psApp/userProfile/userProfileDirective.js
@@ -1,7 +1,16 @@
 ﻿"use strict";
 
-angular.module("psApp").directive("psUserProfile", function () {
+angular.module("psApp").directive("psUserProfile", [function () {
     return {
-        templateUrl: "psApp/userProfile/userProfileTemplate.html"
+        controller: "sho",
+        templateUrl: "psApp/userProfile/userProfileTemplate.html",
+        link: function (scope, el, ctrl, attr) {            
+            
+            scope.shobhit = function () {
+                scope.isVisible = true;
+            };
+    }
     };
-});
+}]).controller("sho", ["$scope", function sho($scope) {
+    $scope.isVisible = false;
+}]);
