202e8a334a52197ddc10e0cb3e7d66c864ca1c8c
Shobhit Varshney <varshneyshobhit98@hotmail.com>
2016-03-01 22:12:16 +0530

    local changes

---
+++ src/PS/wwwroot/ext-modules/newsGrid/newsGridController.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+
+var module = angular.module("newsGrid").controller("newsGridController", [newsGridController]);
+function newsGridController($scope, $http, dataService) {
+   // $scope.data = dataService;
+    $scope.isBusy = true;
+
+    if (dataService) {
+        $scope.isBusy = true;
+        dataService.getNewsGrid()
+         .then(function (result) {
+             //Success
+             debugger;
+         }, function () {
+             //Error
+             alert("Could not load grid data.");
+         }).finally(function () {
+             $scope.isBusy = true;
+         });
+    }
+}
--- src/PS/wwwroot/ext-modules/newsGrid/newsGridDirective.js
+++ src/PS/wwwroot/ext-modules/newsGrid/newsGridDirective.js
@@ -2,8 +2,8 @@
 
 angular.module("newsGrid").directive("newsGrid", function () {
     return {
-        //Not defining the scope because now we want the inherited scope(default scope for angular) for psDashboard 
-        //instead of isolated scope
+        scope:{},
+        controller: "newsGridController",
         templateUrl: "ext-modules/newsGrid/newsGridTemplate.html",
         link: function (scope, el, attrs) {
             //scope.addNewWidget = function (widget) {
