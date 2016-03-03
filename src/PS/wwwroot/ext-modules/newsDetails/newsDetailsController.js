a65eb4a998d34297b6088f3890678c590c5c1a67
vikrant <vikrantnaik01@gmail.com>
2016-03-04 00:52:35 +0530

    newsDetails page check-in

---
+++ src/PS/wwwroot/ext-modules/newsDetails/newsDetailsController.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+var module = angular.module("newsDetails").controller("newsDetailsController", ["$scope", "$http", "dataService", newsDetailsController]);
+
+function newsDetailsController($scope, $http, dataService) {
+    // $scope.data = dataService;
+    // $scope.isBusy = true;
+    if (dataService) {
+        //      $scope.isBusy = true;
+        dataService.getNewsDetails()
+         .then(function (result) {
+             //Success
+             $scope.detailsData = result;
+             console.log(result);
+         }, function () {
+             //Error
+             alert("Could not load details data.");
+         }).finally(function () {
+             //  $scope.isBusy = true;
+         });
+    }
+}
