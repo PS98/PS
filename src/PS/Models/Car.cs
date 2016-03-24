4e37ba06082d81319f09a9aba1e711938a771d0b
jitinjec <erjitinjec@gmail.com>
2016-03-24 19:02:59 +0530

    Car Service Changes

--- src/PS/Models/Car.cs
+++ src/PS/Models/Car.cs
@@ -3,19 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace PS.Models
 {
+    [BsonIgnoreExtraElements]
     public class Car
     {
         public ObjectId _id { get; set; }
         public string name { get; set; }
+        [BsonElement("varient")]
         public List<Varient> varient { get; set; }
     }
     public class Varient
     {
+        [BsonIgnoreIfNull]
         public string name { get; set; }
+        [BsonIgnoreIfNull]
         public string Price { get; set; }
+        [BsonIgnoreIfNull]
         public string Type { get; set; }
     }
 
