4bfc48efba25aa48e327d82ffd9fde999c852993
jitinjec <erjitinjec@gmail.com>
2016-03-28 01:31:58 +0530

    google map and autocomlete

--- src/PS/wwwroot/psApp/carService/script.js
+++ src/PS/wwwroot/psApp/carService/script.js
@@ -10,17 +10,17 @@ var componentForm = {
 };
 
 function initialize() {
-  autocomplete = new google.maps.places.Autocomplete(
-     (document.getElementById('autocomplete')),
-      { types: ['geocode'] });
-  google.maps.event.addListener(autocomplete, 'place_changed', function() {
-    fillInAddress();
-  });
+  //autocomplete = new google.maps.places.Autocomplete(
+  //   (document.getElementById('autocomplete')),
+  //    { types: ['geocode'] });
+  //google.maps.event.addListener(autocomplete, 'place_changed', function() {
+  //  fillInAddress();
+  //});
 }
 
 function fillInAddress() {
   var place = autocomplete.getPlace();
-  console.log( JSON.stringify(place) );
+  console.log(place.geometry.location.lng());
   for (var component in componentForm) {
     document.getElementById(component).value = '';
     document.getElementById(component).disabled = false;
--- src/PS/wwwroot/psApp/carService/selectCentreDirective.js
+++ src/PS/wwwroot/psApp/carService/selectCentreDirective.js
@@ -5,16 +5,11 @@ angular.module("psApp").directive("selectCentre", function () {
         templateUrl: "psApp/carService/selectCentre.html",
         link: function (scope, element, attrs) {
 
-            function get_location() {
-                if (geo_position_js.init()) {
-                    geo_position_js.getCurrentPosition(show_map, handle_error);
-                }
-            }
-            function show_map(position) {
+         function showMap(position) {
                 var lat = position.coords.latitude;
                 var lon = position.coords.longitude;
-                var latlon = new google.maps.LatLng(lat, lon)
-                var mapholder = document.getElementById('mapholder')
+             var latlon = new google.maps.LatLng(lat, lon);
+             var mapholder = document.getElementById('mapholder');
                 mapholder.style.height = '250px';
                 mapholder.style.width = '500px';
 
@@ -34,25 +29,25 @@ angular.module("psApp").directive("selectCentre", function () {
                 infowindow.open(map, marker);
 
             }
-            function handle_error(err) {
+            function handleError(err) {
                 switch (err.code) {
                     case error.PERMISSION_DENIED:
-                        x.innerHTML = "User denied the request for Geolocation."
+                        x.innerHTML = "User denied the request for Geolocation.";
                         break;
                     case error.POSITION_UNAVAILABLE:
-                        x.innerHTML = "Location information is unavailable."
+                        x.innerHTML = "Location information is unavailable.";
                         break;
                     case error.TIMEOUT:
-                        x.innerHTML = "The request to get user location timed out."
+                        x.innerHTML = "The request to get user location timed out.";
                         break;
                     case error.UNKNOWN_ERROR:
-                        x.innerHTML = "An unknown error occurred."
+                        x.innerHTML = "An unknown error occurred.";
                         break;
                 }
             }
 
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(show_map, handle_error);
+                navigator.geolocation.getCurrentPosition(showMap, handleError);
             } else {
                 error('not supported');
             }
