4f29d6a8ec1193c0d5a7da3134756088192733bc
Shobhit Varshney <varshneyshobhit98@hotmail.com>
2016-04-09 03:39:51 +0530

    Privacy and terms page functionality

--- src/PS/wwwroot/ext-modules/index/indexController.js
+++ src/PS/wwwroot/ext-modules/index/indexController.js
@@ -1,6 +1,41 @@
 ﻿"use strict";
 
 angular.module("index").controller("indexController",
-    ["$localStorage", "$location", "$scope",
-        function indexController($localStorage, $location, $scope) {
+    ["$localStorage", "$location", "$scope", "$http", "indexDataService",
+        function indexController($localStorage, $location, $scope, $http, indexDataService) {
+            if (indexDataService) {
+                //      $scope.isBusy = true;
+                indexDataService.getHighlights()
+                 .then(function (result) {
+                     //Success
+                     $scope.highlightsList = result;
+                 }, function () {
+                     //Error
+                     alert("Could not load highlights data.");
+                 }).finally(function () {
+                     //  $scope.isBusy = true;
+                 });
+
+                indexDataService.getPrivacyPolicy()
+                 .then(function (result) {
+                     //Success
+                     $scope.privacyPolicyList = result;
+                 }, function () {
+                     //Error
+                     alert("Could not load privacy policy data.");
+                 }).finally(function () {
+                     //  $scope.isBusy = true;
+                 });
+
+                indexDataService.getTerms()
+                 .then(function (result) {
+                     //Success
+                     $scope.termsList = result;
+                 }, function () {
+                     //Error
+                     alert("Could not load terms data.");
+                 }).finally(function () {
+                     //  $scope.isBusy = true;
+                 });
+            }
  }]);
