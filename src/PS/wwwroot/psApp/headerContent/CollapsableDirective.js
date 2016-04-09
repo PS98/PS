d940956106c07e4fe7d9ed20e243a9ac8ddda4aa
jitinjec <erjitinjec@gmail.com>
2016-04-10 03:03:34 +0530

    select car UI changes

--- src/PS/wwwroot/psApp/headerContent/CollapsableDirective.js
+++ src/PS/wwwroot/psApp/headerContent/CollapsableDirective.js
@@ -5,7 +5,8 @@
             scope: {
                 data: "="
             },
-            link:function(scope,el,attr) {
+            link: function (scope, el, attr) {
+               
                 $("body").delegate(".panel .collapse", "hide.bs.collapse", function () {
                     $(this).parent().find('.fa').toggleClass('fa-plus-circle fa-minus-circle');
                 });
