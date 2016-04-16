b9c9148445ce2a0192fe98f2c2150329009ade09
Shobhit Varshney <varshneyshobhit98@hotmail.com>
2016-04-17 00:03:10 +0530

    contact us functionality

---
+++ src/PS/Models/FeedbackModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PS.Models
+{
+    public class FeedbackModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
