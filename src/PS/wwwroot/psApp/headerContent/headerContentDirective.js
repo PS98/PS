ef5856f04402cdf3e649cd899005d178913ad21a
vikrant <vikrantnaik01@gmail.com>
2016-04-16 18:30:47 +0530

    Back to top button implementaion

--- src/PS/wwwroot/psApp/headerContent/headerContentDirective.js
+++ src/PS/wwwroot/psApp/headerContent/headerContentDirective.js
@@ -6,9 +6,33 @@ angular.module("psApp").directive("headerContent", function () {
         controller:"headerContentController",
         link: function (scope, element, attrs, ctrl) {
             makeSyickyHeader();
+
+            scrollBackToTop();
+
+            $('a.back-to-top').click(function () {
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 700);
+                return false;
+            });
         }
     };
 });
+
+
+function scrollBackToTop() {
+    var amountScrolled = 300;
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('slow');
+        } else {
+            $('a.back-to-top').fadeOut('slow');
+        }
+    });
+}
+
+
+
 function makeSyickyHeader() {
     $(document).ready(function () {
 
