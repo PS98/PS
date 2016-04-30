42a70505104b2964c4153c7d3c197f8314786c84
Shobhit Varshney <varshneyshobhit98@hotmail.com>
2016-05-01 00:37:13 +0530

    user profile dashboard UI and change pass functionality

--- src/PS/wwwroot/ext-modules/dashboard/dashboardController.js
+++ src/PS/wwwroot/ext-modules/dashboard/dashboardController.js
@@ -1,25 +1,64 @@
-﻿angular.module("psApp").controller("dashboardController", ["$scope", "$localStorage", "psDataServices", "psLoginService",
-function ($scope, $localStorage, psDataServices, psLoginService) {
+﻿angular.module("psApp").controller("dashboardController", ["$scope", "$localStorage", "$timeout", "psDataServices", "psLoginService",
+function ($scope, $localStorage, $timeout, psDataServices, psLoginService) {
+
     $scope.userDetails = {};
+
     if (psDataServices.getuserDetails())
         $scope.userDetails = psDataServices.getuserDetails();
+
     $scope.oldNo = $localStorage.userDetails.phoneNo;
-    //$scope.$watch('$scope.userDetails.phoneNo', function () {
-    //    if ($localStorage.userDetails.phoneNo == $scope.userDetails.phoneNo) {
-    //        $scope.otpHide = true;
-    //    } else {
-    //        $scope.otpHide = false;
-    //    }
-    //});
+    $scope.isEdit = false;
+
     $scope.updatePassword = function () {
         var data = { "oldPassword": $scope.oldPwd, "newPassword": $scope.newPwd, "email": $scope.userDetails.email }
-        psLoginService.updatePassword(data)
- .then(function (result) {
-     //Success
-     $scope.result = result;
- }, function (error) {
- });
-        //Error
-
+        if ($scope.newPwd == $scope.reNewPwd) {
+            psLoginService.updatePassword(data)
+             .then(function (result) {
+                 //Success
+                 if (result.status == 1) {
+                     $scope.cngSuccess = true;
+                     $scope.cngError = false;
+                     $scope.successMessage = result.result;
+                 } else if (result.status == 2) {
+                     $scope.cngError = true;
+                     $scope.cngSuccess = false;
+                     $scope.errorMessage = result.result;
+                 }
+             }, function (error) {
+                 //Error
+                 $scope.cngError = true;
+                 $scope.cngSuccess = false;
+                 $scope.errorMessage = result.result;
+             }).finally(function () {
+                 $scope.resetAfterSubmit();
+                 $timeout(function () {
+                     $scope.cngReset();
+                 }, 3000);
+             });
+        }
+        else {
+            $scope.cngSuccess = false;
+            $scope.cngError = true;
+            $scope.resetAfterSubmit();
+            $scope.errorMessage = "Password doesn't match.";
+            $timeout(function () {
+                $scope.cngReset();
+            }, 3000);
+}
     }
+
+    $scope.cngReset = function () {
+        $scope.cngSuccess = false;
+        $scope.cngError = false;
+    }
+
+    $scope.resetAfterSubmit = function () {
+        $scope.oldPwd = null;
+        $scope.newPwd = null;
+        $scope.reNewPwd = null;
+        $scope.changePassword.oldPwd.$dirty = false;
+        $scope.changePassword.newPwd.$dirty = false;
+        $scope.changePassword.reNewPwd.$dirty = false;
+    }
+
 }]);
