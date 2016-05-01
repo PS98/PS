d905d20ab54a6f5e4c4a88e0488de58377b887a7
Shobhit Varshney <varshneyshobhit98@hotmail.com>
2016-05-02 01:41:20 +0530

    pricing template

---
+++ src/PS/wwwroot/psApp/directives/psPricing.js
@@ -0,0 +1,15 @@
+﻿"use strict";
+
+angular.module("psApp").directive("psPricing",
+    [
+        function () {
+            return {
+                scope: {
+
+                },
+                template: "<pricing></pricing>",
+                link: function (scope) {
+
+                }
+            };
+        }]);
