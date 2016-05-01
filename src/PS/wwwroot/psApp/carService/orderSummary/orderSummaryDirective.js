6e1ff20fb17004c5561a5065ddbc5f2a62a1200c
Shobhit Varshney <varshneyshobhit98@hotmail.com>
2016-05-01 19:49:49 +0530

    order success page

--- src/PS/wwwroot/psApp/carService/orderSummary/orderSummaryDirective.js
+++ src/PS/wwwroot/psApp/carService/orderSummary/orderSummaryDirective.js
@@ -6,8 +6,11 @@ angular.module("psApp").directive("orderSummary", function () {
         link: function (scope, element, attrs) {
             //   initializeGoogleMap("", 'mapholder', "", false, scope.MapCallback);
         },
-        controller:["$scope","psDataServices", function ($scope,psDataServices) {
+        controller: ["$scope", "$location", "psDataServices", function ($scope, $location, psDataServices) {
             $scope.orderDetails = psDataServices.getSelectedService();
+            $scope.go = function (path) {
+                $location.path(path);
+            };
         }]
     }
 });
