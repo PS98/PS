8ed13cc28ab703a1d1fccc1b5efcb890e40aac30
Shobhit Varshney <varshneyshobhit98@hotmail.com>
2016-05-06 01:43:31 +0530

    order details UI changes

--- src/PS/wwwroot/ext-modules/dashboard/orderDetailsDirective.js
+++ src/PS/wwwroot/ext-modules/dashboard/orderDetailsDirective.js
@@ -4,8 +4,6 @@ angular.module("psApp").directive("orderDetails", function () {
     return {
         templateUrl: "ext-modules/dashboard/orderDetailsTemplate.html",
         link: function (scope, element, attrs) {
-            debugger;
-
         },
         replace: true,
         scope: {
