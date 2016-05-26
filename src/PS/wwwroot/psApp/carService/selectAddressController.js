bd5483306669ec29ae8626e548d0524c6c5649d7
Shobhit Varshney <varshneyshobhit98@hotmail.com>
2016-05-27 00:44:16 +0530

    merge conflicts fixed

--- src/PS/wwwroot/psApp/carService/selectAddressController.js
+++ src/PS/wwwroot/psApp/carService/selectAddressController.js
@@ -8,92 +8,94 @@ function ($scope, $window, $state, $location, $localStorage, $sessionStorage, ps
         }
     }
 
-    $scope.orderProcessingTC = function(url, name){
+    $scope.orderProcessingTC = function (url, name) {
         window.open(url, name);
     }
 
     $scope.orderProcess = function () {
         $("#ReviewOrder").modal("toggle");
-        if ($scope.payNow) {
-            psDataServices.setPaymentMode("Online");
-               $localStorage.userSelectionData = psDataServices.getSelectedService();
-            $scope.response = psOrderDetailsService.payment()
-                .then(function (result) {
-                    //Success
-                    if (result.status === 0) {
-                        if (result.result != null) {
-                            $window.location.href = result.result.payment_request.longurl;
+        $timeout(function () {
+            if ($scope.payNow) {
+                psDataServices.setPaymentMode("Online");
+                $localStorage.userSelectionData = psDataServices.getSelectedService();
+                $scope.response = psOrderDetailsService.payment()
+                    .then(function (result) {
+                        //Success
+                        if (result.status === 0) {
+                            if (result.result != null) {
+                                $window.location.href = result.result.payment_request.longurl;
+                            }
+                        } else if (result.status === 1 || result.status === 2) {
+                            $scope.reqError = true;
+                            $scope.errorMessage = result.message;
                         }
-                    } else if (result.status === 1 || result.status === 2) {
-                        $scope.reqError = true;
-                        $scope.errorMessage = result.message;
-                    }
-                }, function (error) {
-                    //Error
-                }).finally(function () {
-                });
-        }
-        else {
-            psDataServices.setPaymentMode("COD");
-            $scope.orderSubmit("", "", "");
-        }
+                    }, function (error) {
+                        //Error
+                    }).finally(function () {
+                    });
+            }
+            else {
+                psDataServices.setPaymentMode("COD");
+                $scope.orderSubmit("", "", "");
+            }
+        }, 200);
     }
 
     $scope.orderSubmit = function (payment_id, payment_request_id, result) {
         psOrderDetailsService.submitOrder(payment_id, payment_request_id, result).
             then(function (data) {
-            if (data.status === 0) {
-                $scope.receivedOrder = data.result;
-                psDataServices.resetAll();
-                $localStorage.userData = {};
-                $sessionStorage.orderId = data.result;
+                if (data.status === 0) {
+                    $scope.receivedOrder = data.result;
+                    psDataServices.resetAll();
+                    $localStorage.userData = {};
+                    $sessionStorage.orderId = data.result;
 
-                $state.go("orderSuccess");
-            }
-            else if (data.status === 1 || data.status === 2) {
-                $scope.orderErrorMessage = data.result;
-                $state.go("orderError");
-            }
-        }, function () {
-            alert('Sorry we are unable to process your order. Please try after some time');
-        });
+                    $state.go("orderSuccess");
+                }
+                else if (data.status === 1 || data.status === 2) {
+                    $scope.orderErrorMessage = data.result;
+                    $state.go("orderError");
+                }
+            }, function () {
+                alert('Sorry we are unable to process your order. Please try after some time');
+            });
     }
     var returnData = $location.search();
-    if (returnData.payment_id != null && returnData.payment_request_id !=null){
+    if (returnData.payment_id != null && returnData.payment_request_id != null) {
         psOrderDetailsService.validateOrder(returnData.payment_id, returnData.payment_request_id).success(function (data) {
-            if(data.status == 0 && data.result.success == true)
+            if (data.status == 0 && data.result.success == true)
                 $scope.orderSubmit(returnData.payment_id, returnData.payment_request_id, data.result);
         }).error(function () {
             alert("error");
-        })    
+        })
     }
 
     $scope.ReviewOrder = function () {
         if ($scope.otp == $scope.addressOtp) {
             $scope.otp = undefined;
             if ($scope.addressOtp)
-            $('.mobile_validation').hide();
-                $scope.reviewOrder = [];
-                var order = psDataServices.getSelectedService();
-                $scope.review = true;
-                $scope.custRequest = false;
-                $scope.reviewOrder.push(order);
-                $.each(order.selectedServices, function (i, job) {
-                    if (job.addText) {
-                        $scope.custRequest = true;
-                        $scope.request = job.request;
-                    }
-                });
-                $("#ReviewOrder").modal();
-            }
-            else {
-                $scope.otpError = true;
-                $scope.otpSuccess = false;
-                $scope.errorMessage = "You entered incorrect OTP.";
-                $timeout(function () {
-                    $scope.otpReset();
-                }, 3000);
-            }
+                $('.mobile_validation').hide();
+            $scope.reviewOrder = [];
+            var order = psDataServices.getSelectedService();
+            $scope.review = true;
+            $scope.custRequest = false;
+            $scope.reviewOrder.push(order);
+            $.each(order.selectedServices, function (i, job) {
+                if (job.addText) {
+                    $scope.custRequest = true;
+                    $scope.request = job.request;
+                }
+            });
+            $("#ReviewOrder").modal();
+        }
+        else {
+            $scope.otpError = true;
+            $scope.otpSuccess = false;
+            $scope.errorMessage = "You entered incorrect OTP.";
+            $timeout(function () {
+                $scope.otpReset();
+            }, 3000);
+        }
     }
 
     $scope.requestOtp = function (Mobile) {
