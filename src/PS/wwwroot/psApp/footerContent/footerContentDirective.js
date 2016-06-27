1f5f578f49030e2292ac1e7d3715cf3f388c78e3
jitinjec <erjitinjec@gmail.com>
2016-06-27 21:08:04 +0530

    Removed google map from footer

--- src/PS/wwwroot/psApp/footerContent/footerContentDirective.js
+++ src/PS/wwwroot/psApp/footerContent/footerContentDirective.js
@@ -3,13 +3,15 @@
 angular.module("psApp").directive("footerContent", function () {
     return {
         templateUrl: "psApp/footerContent/footerContentTemplate.html",
-        link: function () {
-            var mapOptions = {
-                center: new google.maps.LatLng("18.5204303", "73.85674369999992"),
-                zoom: 14,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-            }
-            var map = new google.maps.Map(document.getElementById("footer-map"), mapOptions);
+        link: function (scope) {
+            //var mapOptions = {
+            //    center: new google.maps.LatLng("18.5204303", "73.85674369999992"),
+            //    zoom: 14,
+            //    mapTypeId: google.maps.MapTypeId.ROADMAP,
+            //}
+            //var map = new google.maps.Map(document.getElementById("footer-map"), mapOptions);
+
+            scope.mapUrl = "http://maps.googleapis.com/maps/api/staticmap?center=pune&zoom=11&scale=false&size=270x180&maptype=roadmap&format=png&visual_refresh=true";
             $(function () {
                 $('.scroll[href*=#]:not([href=#])').click(function () {
                   //  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
