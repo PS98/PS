aa1789210afa20a48891efe8e07f6019af3f03b2
Viral Dedhia <viralddh@gmail.com>
2016-07-23 23:54:31 +0530

    Added BsonIgnoreExtraElements for customer model

--- src/PS/Models/Customer.cs
+++ src/PS/Models/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace PS.Models
 {
+    [BsonIgnoreExtraElements]
     public class Customer
     {
         public Customer()
